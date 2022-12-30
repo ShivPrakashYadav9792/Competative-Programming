a8ff26633c5bd769e12928ae3a96f67e497010a3
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2022-12-30 20:24:27 +0530

    leetcode problem

---
+++ TOP Question in C++/patrern_using_recursion.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+// function to print a row
+void printn(int num)
+{
+    // base case
+    if (num == 0)
+        return;
+    cout << "* ";
+ 
+    // recursively calling printn()
+    printn(num - 1);
+}
+ 
+// function to print the pattern
+void pattern(int n, int i)
+{
+    // base case
+    if (n == 0)
+        return;
+    printn(i);
+    cout << endl;
+ 
+    // recursively calling pattern()
+    pattern(n - 1,i+1);
+}
+ 
+// driver function
+int main()
+{
+    int n = 5;
+    pattern(n,1);
+    return 0;
+}
