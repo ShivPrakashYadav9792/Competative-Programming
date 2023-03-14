97c8c375c6ed94230f9497aafd6ff886d999e150
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2023-03-14 09:41:21 +0530

    oops

---
+++ Oops/Fraction.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+//#include "Fraction.
+class Fraction {
+	private :
+
+		int numerator;
+		int denominator;
+
+	public :
+
+		Fraction(int numerator, int denominator) {
+			this -> numerator = numerator;
+			this -> denominator = denominator;
+		}
+
+		void print() {
+			cout << this -> numerator << " / " << denominator << endl;
+		}
+
+
+		void simplify() {
+			int gcd = 1;
+			int j = min(this -> numerator, this -> denominator);
+			for(int i = 1; i <= j; i++) {
+				if(this -> numerator % i == 0 && this -> denominator % i == 0) {
+					gcd = i;
+				}
+			}
+			this -> numerator = this -> numerator / gcd;
+			this -> denominator = this -> denominator / gcd;
+		}
+
+		void add(Fraction f2) {
+			int lcm = denominator * f2.denominator;
+			int x = lcm / denominator;
+			int y = lcm / f2.denominator;
+
+			int num = x * numerator + (y * f2.numerator);
+
+			numerator = num;
+			denominator = lcm;
+
+			simplify();	
+
+		}
+};
+int main() {
+	Fraction f1(10, 2);
+	Fraction f2(15, 4);
+
+	f1.add(f2);
+
+	f1.print();
+	f2.print();
+}
+
