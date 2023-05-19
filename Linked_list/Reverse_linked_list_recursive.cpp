d0ed4f39b01a029aa82f1019417482daffdaf4d9
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2023-05-19 17:37:09 +0530

    Linked_Practice.cpp

---
+++ Linked_list/Reverse_linked_list_recursive.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+Node *reverseLinkedListRec(Node *head)
+{
+    //Write your code here
+	if(head==NULL||head->next==NULL)
+		return head;
+		Node *ans=reverseLinkedListRec(head->next);
+		Node *temp=ans;
+                while (temp->next != NULL) {
+                  temp = temp->next;
+                }
+                temp->next=head;
+		head->next=NULL;
+		return ans;
+		
+}
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while(t--)
+	{
+		Node *head = takeinput();
+		head = reverseLinkedListRec(head);
+		print(head);
+	}
+
+	return 0;
+}
