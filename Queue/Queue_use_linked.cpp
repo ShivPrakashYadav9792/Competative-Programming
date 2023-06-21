aaa8a84566ad27759e60c79599715254c921dd97
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2023-06-21 19:02:08 +0530

    Queue using linked list

---
+++ Queue/Queue_use_linked.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include "queue_using_linked_list.h"
+
+int main() {
+	Queue<int> q;
+
+	q.enqueue(10);
+	q.enqueue(20);
+	q.enqueue(30);
+	q.enqueue(40);
+	q.enqueue(50);
+	q.enqueue(60);
+
+
+	cout << q.front() << endl;
+	cout << q.dequeue() << endl;
+	cout << q.dequeue() << endl;
+	cout << q.dequeue() << endl;
+
+	cout << q.getSize() << endl;
+	cout << q.isEmpty() << endl;
+}
+
