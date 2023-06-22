311c526de8777967b81c4630dfab81a56a508560
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2023-06-22 19:21:44 +0530

    Reverse_Queue_using_recursion

---
+++ Queue/Reverese_Queue_using_recursion.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include<queue>
+void reverseQueue(queue<int>&input){
+    if(input.size()<=1)
+    return ;
+    int temp=input.front();
+    input.pop();
+    reverseQueue(input);
+    input.push(temp);
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        queue<int> q;
+        int size;
+        cin >> size;
+
+        for (int i = 0, val; i < size; i++) {
+            cin >> val;
+            q.push(val);
+        }
+
+        reverseQueue(q);
+        while (!q.empty()) {
+            cout << q.front() << " ";
+            q.pop();
+        }
+
+        cout << "\n";
+    }
+}
