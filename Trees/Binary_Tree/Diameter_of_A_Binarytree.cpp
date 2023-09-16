6f219e91237a3f91d0fbaec88b55d60829f7ee93
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2023-09-16 13:46:59 +0530

    Tree_Practice_Problems

--- Trees/Binary_Tree/Diameter_of_A_Binarytree.cpp
+++ Trees/Binary_Tree/Diameter_of_A_Binarytree.cpp
@@ -42,27 +42,53 @@ BinaryTreeNode<int> *takeInputLevelWise()
     }
     return root;
 }
-int height(BinaryTreeNode<int> *root)
+
+
+pair<int, int> heightDiameter(BinaryTreeNode<int> *root)
 {
     if (root == NULL)
     {
-        return 0;
+        pair<int, int> p;
+        p.first = 0;
+        p.second = 0;
+        return p;
     }
-    return 1 + max(height(root->left), height(root->right));
+    pair<int, int> leftAns = heightDiameter(root->left);
+    pair<int, int> rightAns = heightDiameter(root->right);
+    int ld = leftAns.second;
+    int lh = leftAns.first;
+    int rd = rightAns.second;
+    int rh = rightAns.first;
+
+    int height = 1 + max(lh, rh);
+    int diameter = max(lh + rh, max(ld, rd));
+    pair<int, int> p;
+    p.first = height;
+    p.second = diameter;
+    return p;
 }
 
-int diameter(BinaryTreeNode<int> *root)
-{
-    if (root == NULL)
-    {
-        return 0;
-    }
+// int height(BinaryTreeNode<int> *root)
+// {
+//     if (root == NULL)
+//     {
+//         return 0;
+//     }
+//     return 1 + max(height(root->left), height(root->right));
+// }
 
-    int option1 = height(root->left) + height(root->right);
-    int option2 = diameter(root->left);
-    int option3 = diameter(root->right);
-    return max(option1, max(option2, option3));
-}
+// int diameter(BinaryTreeNode<int> *root)
+// {
+//     if (root == NULL)
+//     {
+//         return 0;
+//     }
+
+//     int option1 = height(root->left) + height(root->right);
+//     int option2 = diameter(root->left);
+//     int option3 = diameter(root->right);
+//     return max(option1, max(option2, option3));
+// }
 
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
 int main()
@@ -80,8 +106,8 @@ int main()
     */
     BinaryTreeNode<int> *root = takeInputLevelWise();
     // printTree(root);
-    cout << "Num: " << numNodes(root) << endl;
-    inorder(root);
-    cout << endl;
+    pair<int, int> p = heightDiameter(root);
+    cout << "Height: " << p.first << endl;
+    cout << "Diameter: " << p.second << endl;
     delete root;
 }
